226925d1610c142edfad29e62bb9d458945861f0
Ignacio Velazquez <ignaciov92@gmail.com>
2022-11-13 18:01:49 -0300

    complete ejercicio 2 funciones

--- apps/funciones2.js
+++ apps/funciones2.js
@@ -2,3 +2,17 @@
 le pasa como argumento. A partir de la cadena que se le pasa, la función
 determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o
 por una mezcla de ambas */
+
+
+
+// const validar=(texto) => {
+//   if (texto == texto.toLowerCase()) {
+//     document.write(`El texto contiene solo Minusculas`)
+//   } else if (texto == texto.toUpperCase()){
+//     document.write(`El texto contiene solo Mayusculas`)
+//   } else {
+//     document.write(`El texto contiene una combinación de minusculas y mayusculas`)
+//   }
+// }
+
+// validar (prompt (`ìngresa un texto= `))
