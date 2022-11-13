cb7895ca63d255d7454aacd6dc2d4bd3a6644d99
Ignacio Velazquez <ignaciov92@gmail.com>
2022-11-13 18:47:46 -0300

    resuelto 3er ejercicio de funciones

--- apps/funciones3.js
+++ apps/funciones3.js
@@ -2,3 +2,11 @@
 luego crear una función para calcular su perímetro y mostrarlo por pantalla.
 ● La fórmula del perímetro es p = 2*(a +b)
 */
+// let alto = parseInt(prompt("ingresa el alto del rectangulo= "))
+// let ancho = parseInt(prompt("ingresa el alto del rectangulo= "))
+
+// const calculadorPerimetro = (alto , ancho) => {
+//   return `El perimetro es ${2*(alto + ancho)} `; 
+// }
+
+// document.write(calculadorPerimetro(alto,ancho))
