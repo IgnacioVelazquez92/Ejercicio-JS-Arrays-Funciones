050dc19b53901bbd46d1b139c6f9321f81b32f97
Ignacio Velazquez <ignaciov92@gmail.com>
2022-11-13 19:05:27 -0300

    final del practico

--- apps/funciones4.js
+++ apps/funciones4.js
@@ -1,3 +1,14 @@
 /*4- Escriba un script que muestre la tabla de multiplicar de un número ingresado
 por pantalla, la creación de la tabla debe ser realizada con una función y mostrar
 solo los resultados del 1 al 10 del número elegido por el usuario. */
+
+// num =parseInt ( prompt("ingresa un número ="))
+
+// const tablaMult = (num) => {
+//   document.write(`<h3>la tabla del ${num} es:</h3><br>`)
+//   for (i=1; i<=10 ;i++){
+//     document.write (`${i} x ${num} = <b>${i * num} </b><br>`)
+//   }
+// }
+
+// tablaMult(num)
